6428ccce42b86bd8d5f6c6bc2323410e6d420bd8
Daniel Hood <mrdhood@gmail.com>
2020-07-10 15:43:04 -0400

    php code sniffer changes and removing analytics from the Countries class

--- src/Countries.php
+++ src/Countries.php
@@ -4,42 +4,33 @@ namespace Dch\Covid;
 
 use Throwable;
 
-class Countries {
+class Countries
+{
+    private const OWID_WRL = 'OWID_WRL';
+
     private $countries = [];
+    private $owid_wrl;
     private $averages = [];
 
-    public function __construct(array $data) {
+    public function __construct(array $data)
+    {
         foreach ($data as $countryCode => $country) {
+            if ($countryCode === self::OWID_WRL) {
+                $this->owid_wrl = new Country($country);
+                continue;
+            }
+
             $this->countries[$countryCode] = new Country($country);
         }
     }
 
-    public function getCountryCodes(): array {
+    public function getCountryCodes(): array
+    {
         return array_keys($this->countries);
     }
 
-    public function compileAveragesDaysAfterFirst(): void {
-        foreach (array_keys($this->countries) as $countryCode) {
-            $country = $this->getCountry($countryCode);
-            $dayIndex = 0;
-
-            try {
-                while(true) {
-                    $dayIndex++;
-                    $dailyStat = $country->getDayByOffset($dayIndex);
-                    if (is_null($dailyStat)) {
-                        break;
-                    }
-
-                    $this->averages[$dayIndex]['cases'][] = $dailyStat->getTotalCasesPerMillion();
-                    $this->averages[$dayIndex]['deaths'][] = $dailyStat->getTotalDeathsPerMillion();
-                }
-            } catch (Throwable $e) {
-            }
-        }
-    }
-
-    public function getCountry(string $countryCode): Country {
+    public function getCountry(string $countryCode): Country
+    {
         if (!isset($this->countries[$countryCode])) {
             throw new Exception("No data found for " . $countryCode);
         }
@@ -47,20 +38,8 @@ class Countries {
         return $this->countries[$countryCode];
     }
 
-    public function getDifferenceFromAverageForCountry(string $countryCode, string $type, int $offset): float {
-        $countryStat = $this->getCountry($countryCode)->getDayByOffset($offset);
-        
-        $stats = [
-            'cases' => $countryStat->getTotalCasesPerMillion(),
-            'deaths' => $countryStat->getTotalDeathsPerMillion()
-        ];
-        
-        $worldAverage = $this->getWorldWideAverages($type, $offset);
-
-        return $stats[$type] - $worldAverage;
-    }
-
-    public function getWorldWideAverages(string $type, int $offset): float {
+    public function getWorldWideAverages(string $type, int $offset): float
+    {
         $values = array_filter($this->averages[$offset][$type]);
         $average = array_sum($values) / count($values);
 
